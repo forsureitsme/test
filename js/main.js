16ef8c0db4ae86777414524608b24f84ed4b2d04
Pedro Cardoso da Silva <forsureitsme@gmail.com>
2014-12-25 22:34:28 -0200

    Alert test

--- js/main.js
+++ js/main.js
@@ -14,9 +14,20 @@ var app = {
     },
 
     initialize: function() {
-        this.store = new MemoryStore();
+        this.store = new MemoryStore(function()
+        {
+            app.showAlert( 'Store is up!', 'Info'  );
+        });
         $('.search-key').on('keyup', $.proxy(this.findByName, this));
-    }
+    },
+
+    showAlert: function (message, title) {
+        if (navigator.notification) {
+            navigator.notification.alert(message, null, title, 'OK');
+        } else {
+            alert(title ? (title + ": " + message) : message);
+        }
+    },
 
 };
 
